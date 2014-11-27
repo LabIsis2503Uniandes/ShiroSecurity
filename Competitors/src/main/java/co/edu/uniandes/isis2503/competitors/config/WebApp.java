c8741fb620cf6f3f8ad7736383e0ffdcea49def6
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2014-11-27 12:22:47 -0500

    Solucion Bug Index.html

--- Competitors/src/main/java/co/edu/uniandes/isis2503/competitors/config/WebApp.java
+++ Competitors/src/main/java/co/edu/uniandes/isis2503/competitors/config/WebApp.java
@@ -3,7 +3,7 @@ package co.edu.uniandes.isis2503.competitors.config;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("/")
+@ApplicationPath("/webresources")
 public class WebApp extends ResourceConfig {
 	public WebApp(){
         packages("co.edu.uniandes.isis2503.competitors.service");
