7de7dc384366a538317da3a59d7bc70d813d2ec7
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2014-12-09 13:12:50 -0500

    LDAP con libreria externa Shiro

--- Competitors/src/main/java/co/edu/uniandes/isis2503/user/service/UserService.java
+++ Competitors/src/main/java/co/edu/uniandes/isis2503/user/service/UserService.java
@@ -11,6 +11,9 @@ import co.edu.uniandes.csw.miso4204.security.logic.SecurityLogic;
 import co.edu.uniandes.csw.miso4204.security.logic.dto.UserDTO;
 import co.edu.uniandes.isis2503.competitors.logic.CompetitorsLogic;
 import com.google.gson.Gson;
+import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.api.ApiKey;
+import com.stormpath.sdk.api.ApiKeys;
 import javax.annotation.PostConstruct;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -18,6 +21,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import com.stormpath.sdk.tenant.*;
+import com.stormpath.sdk.application.*;
+import com.stormpath.sdk.authc.AuthenticationRequest;
+import com.stormpath.sdk.authc.AuthenticationResult;
+import com.stormpath.sdk.authc.UsernamePasswordRequest;
+import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.client.Clients;
+import com.stormpath.sdk.group.Group;
+import com.stormpath.sdk.group.GroupCriteria;
+import com.stormpath.sdk.group.GroupList;
+import com.stormpath.sdk.group.Groups;
+import com.stormpath.sdk.resource.ResourceException;
 
 /**
  *
@@ -30,33 +45,47 @@ public class UserService {
 
     protected SecurityLogic securityLogic;
 
-    
-    
     @PostConstruct
-	public void loadDependencies(){
-		securityLogic = new SecurityLogic();
-	} 
-    
-    
+    public void loadDependencies() {
+        securityLogic = new SecurityLogic();
+    }
+
     @Path("/login")
     @POST
     public Response login(UserDTO user) {
         int status = 500; //Codigo de error en el servidor
         String token = "User and/or password wrong";
-        
-        try{
-            UserDTO userDb = securityLogic.getUserSession(user.getUsername());
-            if(userDb!=null){
-                if(userDb.getUsername().equals(user.getUsername()) && userDb.getPassword().equals(user.getPassword())){
-                    token = new Gson().toJson(JsonWebToken.encode(userDb,"Un14nd3s2014@" ,JwtHashAlgorithm.HS256));
-                    status = 200;
-                }
-            
-            }
-           
-        }catch(Exception e){
-            e.printStackTrace();
-        
+        UserDTO userStorm = new UserDTO();
+        String path = "C:\\Users\\Jj.alarcon10\\Documents\\apiKey.properties";//Colocar la Ubicacion de su archivo apiKey.properties
+        ApiKey apiKey = ApiKeys.builder().setFileLocation(path).build();
+        Client client = Clients.builder().setApiKey(apiKey).build();
+
+        try {
+            AuthenticationRequest request = new UsernamePasswordRequest(user.getUsername(), user.getPassword());
+            Tenant tenant = client.getCurrentTenant();
+            ApplicationList applications = tenant.getApplications(Applications.where(Applications.name().eqIgnoreCase("My Application")));
+            Application application = applications.iterator().next();
+
+            AuthenticationResult result = application.authenticateAccount(request);
+            Account account = result.getAccount();
+            userStorm.setEmail(account.getEmail());
+            userStorm.setName(account.getFullName());
+            userStorm.setUsername(account.getUsername());
+            userStorm.setPassword(user.getPassword());
+            userStorm.setLevelAccess("Admin");
+            token = new Gson().toJson(JsonWebToken.encode(userStorm, "Un14nd3s2014@", JwtHashAlgorithm.HS256));
+            status = 200;
+
+//          UserDTO userDb = securityLogic.getUserSession(user.getUsername());
+//          if(userDb!=null){
+//                if(userDb.getUsername().equals(user.getUsername()) && userDb.getPassword().equals(user.getPassword())){
+//                    token = new Gson().toJson(JsonWebToken.encode(userDb,"Un14nd3s2014@" ,JwtHashAlgorithm.HS256));
+//                    status = 200;
+//                }
+//            
+//            }
+        } catch (ResourceException ex) {
+            System.out.println(ex.getStatus() + " " + ex.getMessage());
         }
 
         return Response.status(status).entity(token).build();
---
+++ Competitors/src/main/resources/META-INF/dataExample.sql
@@ -0,0 +1,28 @@
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner) 
+	VALUES (20, 'Calle falsa 123', 20, '31540450', 'Bogota', 'Colombia', '2014-12-09', 'Kay', 'Maria', '123445', '2014-12-09', false);
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner) 
+	VALUES (21, 'Calle falsa 123', 20, '31121240450', 'Bogota', 'Colombia', '2014-12-09', 'Emily', 'Maria', '123445', '2014-12-09', false);
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (22, 'Calle falsa 123', 20, '311254012450', 'Bogota', 'Colombia', '2014-12-09', 'Juan', 'Mario', '123445', '2014-12-09', false);
+
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (23, 'Calle falsa 123', 20, '311212550', 'Bogota', 'Colombia', '2014-12-09', 'Alexander', 'Mario', '123445', '2014-12-09', false);
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (24, 'Calle falsa 123', 20, '31120450', 'Bogota', 'Colombia', '2014-12-09', 'Kamel', 'Mario', '123445', '2014-12-09', false);
+
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (25, 'Calle falsa 123', 20, '31140450', 'Bogota', 'Colombia', '2014-12-09', 'Kairy', 'Maria', '123445', '2014-12-09', false);
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (26, 'Calle falsa 123', 20, '3112450', 'Bogota', 'Colombia', '2014-12-09', 'Carlos', 'Mario', '123445', '2014-12-09', false);
+
+insert INTO "public".competitorentity (id, address, age, cellphone, city, country, create_at, "name", surname, telephone, updated_at, winner)
+	VALUES (27, 'Calle falsa 123', 20, '31140450', 'New York', 'United States', '2014-12-09', 'Andrew', 'Stevenson', '123445', '2014-12-09', false);
+
+
